4b874b7f80ea68b1ed2f78ba7467df8441cd5901
Erik Burton <erikburton1@gmail.com>
2025-10-18 15:41:38 -0600

    Started working through chapter 2.

---
+++ Chapter2/scratch.py
@@ -0,0 +1,30 @@
+grocery_list = ["eggs", "milk", "cheese", "pasta"]
+
+print("The first item on the list is " + grocery_list[0])
+print("The second item on the list is " + grocery_list[1])
+
+# random_assortment = ["egg", "tree", 3, "green", 94, "pluto", 3.14]
+# print("\nThe third item is " + random_assortment[int(2)])
+
+#  Tuples are imutable (can't be edited) and use parenthesis not brackets
+planets = (
+    "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn",
+    "Uranus", "Neptune")
+print(planets[3])
+
+#  Sets are surrounded by curley brackets {}
+customers = {
+        "James Smith",
+        "Andrea Richards",
+        "Sam Sharp",
+        "Brenda Longmire",
+        "Veronica March",
+        "Sylvia Smith",
+        "James Smith",
+        "Vanessa Bush",
+        "Steve Hammersmith",
+        "Brenda Longmire",
+        "Sylvia Smith",
+        "Steve Hammersmith",
+        "Walt Hawkins"
+        }
---
+++ dictionaries.py
@@ -0,0 +1,19 @@
+#  customer1 and customer2 are dictionay variables
+customer1 = {
+    "name": "James Smith",  # This is a value of the dictionary.
+    "age": 24,  # The values are referenced by key-value pairs
+    "phone": "555-555-1941",
+    "email": "email@example.com"
+}
+
+customer2 = {
+    "name": "Andrea Richards",
+    "age": 35,
+    "phone": "555-555-1942",
+    "email": "email2@example.com"
+}
+
+print(f"\nThis is Customer1's name: {customer1['name']}")
+print(f"\nThis is Customer2's email: {customer2['email']}")
+print(f"\nThis is Customer1's info: {customer1}")
+print(f"\nThis is Customer2's info: {customer2}")
