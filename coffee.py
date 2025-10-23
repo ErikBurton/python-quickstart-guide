bc883eb01a8da47b4067e5ac0b9acf80fe880775
Erik Burton <erikburton1@gmail.com>
2025-10-22 20:13:16 -0600

    Completes chp5, functions, pass arguments, default args and keyword args for coffee.py

--- coffee.py
+++ coffee.py
@@ -42,21 +42,48 @@ coffee = 100
 sales = []
 
 #  Print welcome message
-print("\nClydeBank Coffee Shop Simulator 2112, Version 1.00")
-print("Copyright (C) 2025 ClydeBank Media, All Rights Reserved.\n")
-print("Let's collect some information before we start the game.\n")
+def welcome():
+    print("\nClydeBank Coffee Shop Simulator 2112, Version 1.00")
+    print("Copyright (C) 2025 ClydeBank Media, All Rights Reserved.\n")
+    print("Let's collect some information before we start the game.\n")
 
 #  Get name and shop name using the following approach.
 # 1. Set name and shop_name to False
 # 2. Use while not name and shop_name to continue to prompt for a non-empty string.
 
-name = False
-while not name:
-    name = input("What is your name? ")
+def prompt(display="Please input a string", require=True):
 
-shop_name = False
-while not shop_name:
-    shop_name = input("What do you want to name your coffee shop? ")
+    if require:
+        s = False
+        while not s:
+            s = input(display + " ")
+    else:
+        s = input(display + " ")
+    return s
+
+def daily_stats(cash_on_hand, weather_temp, coffee_inventory):
+    print("You have $" + str(cash_on_hand) + " cash on hand and the temperature is " + str(weather_temp) + ".")
+    print("You have enough coffee on hand to make " + str(coffee_inventory) + " cups.\n")
+
+def convert_to_float(s):
+    # If convervion fails, assign 0 to i
+    try:
+        f = float(s)
+    except ValueError:
+        f = 0
+    return f
+
+def get_weather():
+    # Generate a random temperature between 20 and 90
+    # We'll consider seasons later on, but this is good enough for now
+    return randint(20,90)
+
+# Print Welcome message
+welcome()
+
+# Get name and store name
+name = prompt("What is your name?", True)
+shop_name = prompt("What do you want to name your coffee shop?", True)
 
 # We have what we need, so let's get started!
 print("\nOK, let's get started. Have fun!")
@@ -67,13 +94,10 @@ while running:
     # Display the day and add a "fancy" text effect.
     print("\n-----| Day " + str(day) + " @ " + shop_name + " |-----")
 
-    # Generate a random temperature between 20 and 90
-    # We'll consider seasons later on, but his is good enough for now
-    temperature = randint(20, 90)
+    temperature = get_weather()
 
     # Display the cash and weather
-    print("You have $" + str(cash) + "cash and it's " + str(temperature) + " degress.")
-    print("You have enough coffee on hand to make " + str(coffee) + " cups.\n")
+    daily_stats(cash, temperature, coffee)
 
     # Get the prince of a cup of coffee
     cup_price = input("What do you want to charge per cup of coffee? ")
