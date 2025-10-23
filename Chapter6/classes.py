3418300b84e8afb832fe0ed0e3bf2bb74fba71f4
Erik Burton <erikburton1@gmail.com>
2025-10-22 20:18:54 -0600

    clsss World().

---
+++ Chapter6/classes.py
@@ -0,0 +1,13 @@
+# Define the World Class
+class World:
+    # Define the greeting
+    greeting = "Hello, World!"
+
+    # Run this whenever the object is creted
+    def __init__(self):
+        # Print the greeting
+        print(self.greeting)
+
+
+# Use the class World to crete a world object nameed w
+w = World()
