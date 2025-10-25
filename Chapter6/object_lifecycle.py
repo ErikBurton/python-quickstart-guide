340953ed9edf4fef2f8f91952fbb22b5138f63dc
Erik Burton <erikburton1@gmail.com>
2025-10-24 19:46:13 -0600

    enter and exit methods.

--- Chapter6/object_lifecycle.py
+++ Chapter6/object_lifecycle.py
@@ -1,25 +1,49 @@
-class World:
+# class World:
 
     # Define out greeting
-    greeting = "Hello, World!"
+    # greeting = "Hello, World!"
 
     # Run this whenever the object is created
-    def __init__(self):
-        # Print the greeting
-        print(self.greeting)
+    # def __init__(self):
+    #     # Print the greeting
+    #     print(self.greeting)
 
 
 # Define a new class
-class Customer:
+# class Customer:
 
     # Define the init method, using name and city as arguments
-    def __init__(self, name, city):
-        self.name = name
-        self.city = city
+    # def __init__(self, name, city):
+    #     self.name = name
+    #     self.city = city
 # Create three objects based on the Customer class
 # The name and city are passed to __ init __
 
 
-c1 = Customer("Sarah", "Atlanta")
-c2 = Customer("Robert", "Florence")
-c3 = Customer("Thomas", "Denver")
+# c1 = Customer("Sarah", "Atlanta")
+# c2 = Customer("Robert", "Florence")
+# c3 = Customer("Thomas", "Denver")
+
+
+# Define a new class
+class Customer:
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+
+    def __enter__(self):
+        print("Entering scope.")
+        # Run code upon entereing scope of with statement
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("Leaving scope.")
+        # Run code upon leaving scope of width statement
+
+    def greet(self):
+        print("Hello, " + self.name + "!")
+
+
+# Use with to create a scope
+with Customer("Erik", "Colton") as erik:
+    erik.greet()
